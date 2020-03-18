f39fefb5581119f6d2c1648297c4fd936bac3c59
Alfred-Lau <jian.liu@icloud.com>
2020-03-18 22:54:59 +0800

    Context & ContextType

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BatteryContext = createContext();
+/* 
+class Leaf extends Component {
+  static contextType = BatteryContext;
+  render() {
+    return (
+      <BatteryContext.Consumer>
+        {value => {
+          return <h1> Battery: {value}</h1>;
+        }}
+      </BatteryContext.Consumer>
+    );
+  }
+} */
+
+class Leaf extends Component {
+  static contextType = BatteryContext;
+  render() {
+    const battery = this.context;
+    return <h1> Battery: {battery}</h1>;
+  }
+}
+class Middle extends Component {
+  render() {
+    return <Leaf></Leaf>;
+  }
+}
+
+class App extends Component {
+  state = {
+    Battery: 60
+  };
+  render() {
+    return (
+      <div className='App'>
+        <BatteryContext.Provider value={this.state.Battery}>
+          <button
+            onClick={() => this.setState({ Battery: this.state.Battery - 1 })}
+          >
+            press me
+          </button>
+          <Middle></Middle>
+        </BatteryContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
