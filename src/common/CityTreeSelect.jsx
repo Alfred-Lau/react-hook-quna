d95ae58698720944e48eec1fc495ca34db4bc62f
Alfred-Lau <jian.liu@icloud.com>
2020-06-03 20:53:52 +0800

    feat:style optimation

--- src/common/CityTreeSelect.jsx
+++ src/common/CityTreeSelect.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import './cityTreeSelect.css';
 
+function CityShowArea(props) {
+  const { hotSpot } = props;
+  return (
+    <div className="city-show-area">
+      <h5 className="city-show-title">定位/历史</h5>
+      <div className="city-show content">
+        <input type="button" className="location" value="定位"></input>
+      </div>
+      <h5 className="city-show-title">热门</h5>
+      <div className="city-show content">热门</div>
+    </div>
+  );
+}
+
 export default function CityTreeSelect(props) {
   const { cityData, cityVisible, handleSelect, goBack } = props;
+
+  const [searchKey, setSearchKey] = useState('');
 
   const citySelector = classnames('city-selector', {
     hidden: !cityVisible,
   });
   return (
     <div className={citySelector}>
-      <div className="city-search-header">
-        <span className="goBack" onClick={goBack}>
+      <div className="city-search">
+        <div className="search-back" onClick={() => goBack()}>
           <svg width="42" height="42">
             <polyline
               points="25,13 16,21 25,29"
@@ -22,17 +38,26 @@ export default function CityTreeSelect(props) {
               fill="none"
             />
           </svg>
-        </span>
-        <div className="city-search-box">
-          <i class="iconfont search-icon">&#xe632;</i>
+        </div>
+        <div className="search-input-wrapper">
           <input
             type="text"
-            className="search-content"
-            placeholder="城市车站的中文或拼音"
+            value={searchKey}
+            className="search-input"
+            placeholder="城市、车站的中文或拼音"
+            onChange={(e) => setSearchKey(e.target.value)}
           />
-          <i className="iconfont close-icon">&#xe62a;</i>
         </div>
+        <i
+          onClick={() => setSearchKey('')}
+          className={classnames('search-clean', {
+            // hidden: key.length === 0,
+          })}
+        >
+          &#xf063;
+        </i>
       </div>
+      <CityShowArea hotSpot={[]}></CityShowArea>
     </div>
   );
 }
